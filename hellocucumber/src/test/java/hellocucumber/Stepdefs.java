971cb12cdda1af9f724d6ea0e6439eceecd6843b
Gupta <ngupta@c02sj1kafvh8.agoda.local>
2019-04-23 17:57:03 +0700

    Failed the test

--- hellocucumber/src/test/java/hellocucumber/Stepdefs.java
+++ hellocucumber/src/test/java/hellocucumber/Stepdefs.java
@@ -1,28 +1,33 @@
 package hellocucumber;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
 
 import static org.junit.Assert.*;
 
+class IsItFriday {
+    static String isItFriday(String today) {
+        return null;
+    }
+}
+
 public class Stepdefs {
+    private String today;
+    private String actualAnswer;
+
     @Given("^today is Sunday$")
     public void today_is_Sunday() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        today = "Sunday";
     }
 
     @When("^I ask whether it's Friday yet$")
     public void i_ask_whether_it_s_Friday_yet() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+        actualAnswer = IsItFriday.isItFriday(today);
     }
 
     @Then("^I should be told \"([^\"]*)\"$")
-    public void i_should_be_told(String arg1) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
     }
 }
